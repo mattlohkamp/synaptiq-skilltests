c1dcdd196679eb95e23d5be7878ec3170a5dd600
mattlohkamp <work@mattlohkamp.com>
2023-08-27 23:20:41 -0700

    make the page look nicer

--- react/pages/HolidayShoppingCountdown.tsx
+++ react/pages/HolidayShoppingCountdown.tsx
@@ -1,44 +1,48 @@
-import "@/app/globals.css";
+import { ChangeEvent, useState } from "react";
+import Image from "next/image";
 import { getFutureDateDifferenceMs } from "@/common/utils";
 import DatePickerList, {
 	DatePickerListOption,
 } from "@/components/DatePickerList";
+import "@/app/globals.css";
 import convert from "convert";
-import Image from "next/image";
-import { ChangeEvent, useState } from "react";
+import Head from "next/head";
 
 const mapHolidaysToOptions = ({ name }: { name: string }, i: number) => ({
 	label: name,
-	value: i + 1, //	increment to account for empty option
+	value: i + 1, //	increment to account for initial empty option
 });
 
 export default function HolidayShoppingCountdown() {
 	const presentDateAtInit = new Date();
 	const holidays = [
 		{
+			name: "New Years Eve",
+			date: "1/1",
+		},
+		{
+			name: "Valentines Day",
+			date: "2/14",
+		},
+		{
 			name: "Easter",
 			date: "3/31",
-			giftListKey: "easter",
 		},
 		{
 			name: "Mother's Day",
 			date: "5/12",
-			giftListKey: "mother",
 		},
 		{
 			name: "Father's Day",
 			date: "6/16",
-			giftListKey: "father",
 		},
 		{
 			name: "Matt's Birthday",
 			date: "8/7",
-			giftListKey: "birthday",
 		},
 		{
 			name: "Christmas",
 			date: "12/25",
-			giftListKey: "christmas",
 		},
 	];
 	const options: DatePickerListOption[] = [
@@ -59,48 +63,52 @@ export default function HolidayShoppingCountdown() {
 	}
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="w-full max-w-3xl items-center justify-between text-xl lg:flex">
-				<Image
-					src="https://y.yarn.co/026b65c5-9ada-46b8-b1ec-11135f7efe41_text.gif"
-					alt="There's only 365 days left till next Halloween!"
-					priority
-					width={400}
-					height={240}
-				/>
-				<p>
-					Do you have your shopping done yet?
-					<br />
-					<em className="italic">How long do you have left??</em>
-					<br />
-					Let&apos;s find out...
-				</p>
-			</div>
-			<div className="w-full max-w-3xl items-center justify-center text-xl lg:flex m-8">
-				<DatePickerList
-					options={options}
-					onChange={datePickerListOnChange}
-					selectedIndex={selectedIndex}
-					labelText="How many days left to shop for... "
-				/>
-			</div>
-			{selectedIndex > 0 ? (
-				<p className="text-center text-2xl lg:flex">
-					There&apos;s only&nbsp;
-					<strong className="inline">
-						{Math.floor(
-							convert(
-								getFutureDateDifferenceMs(
-									holidays[selectedIndex - 1].date,
-									presentDateAtInit
-								),
-								"ms"
-							).to("days")
-						)}
-					</strong>
-					&nbsp;days left till next {holidays[selectedIndex - 1].name}!
-				</p>
-			) : null}
-		</main>
+		<>
+			<Head>
+				<title>Holiday Shopping Countdown</title>
+			</Head>
+			<main id="container" className="p-16 text-center w-full max-w-4xl">
+				<div className="bg-black rounded-2xl bg-opacity-80 p-8">
+					<h1 className="text-4xl my-8">Do you have your shopping done yet?</h1>
+					<p className="text-xl">
+						Let&apos;s find out how long you have left...
+					</p>
+					<div className="my-8">
+						<DatePickerList
+							options={options}
+							onChange={datePickerListOnChange}
+							selectedIndex={selectedIndex}
+							labelText="How many days left to shop for "
+						/>
+					</div>
+					{selectedIndex > 0 ? (
+						<>
+							<p className="text-3xl my-6">
+								There&apos;s only&nbsp;
+								<strong className="inline">
+									{Math.ceil(
+										convert(
+											getFutureDateDifferenceMs(
+												holidays[selectedIndex - 1].date,
+												presentDateAtInit
+											),
+											"ms"
+										).to("days")
+									)}
+								</strong>
+								&nbsp;days left till next {holidays[selectedIndex - 1].name}!
+							</p>
+						</>
+					) : null}
+				</div>
+			</main>
+			<style jsx>
+				{`
+					#container {
+						background: url("/istockphoto-496569618-612x612.jpg");
+					}
+				`}
+			</style>
+		</>
 	);
 }
