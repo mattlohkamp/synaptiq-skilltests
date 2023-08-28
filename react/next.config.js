89ca6fbeec39a6cbdad768316676433a9a4b4f92
mattlohkamp <work@mattlohkamp.com>
2023-08-27 22:59:01 -0700

    dont need the image src config

--- react/next.config.js
+++ react/next.config.js
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				//	ex. https://y.yarn.co/026b65c5-9ada-46b8-b1ec-11135f7efe41_text.gif
-				protocol: "https",
-				hostname: "y.yarn.co",
-				pathname: "/**.gif",
-			},
-		],
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
